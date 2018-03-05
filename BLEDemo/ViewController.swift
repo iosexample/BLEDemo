de4dc60360d3f9dbbb1623ce83033dec9c9d2dff
Dong <donly@magicalboy.com>
2018-03-05 14:23:38 +0800

    Initial Commit

---
+++ BLEDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLEDemo
+//
+//  Created by dong on 5/3/2018.
+//  Copyright © 2018 dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
